5b1508c0d70d2b19a0b331b6e421e159db7c0523
y.matytsin <y.matytsin@vk.team>
2024-10-05 14:40:22 +0300

    проверка кода 02

--- ftracker.go
+++ ftracker.go
@@ -104,7 +104,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
     // ваш код здесь
-    return (walkingCaloriesWeightMultiplier * weight + (math.Pow(meanSpeed(action, duration) * 3600, 2) / height) * walkingSpeedHeightMultiplier * weight) * duration * minInH
+    return (walkingCaloriesWeightMultiplier * weight + (math.Pow(meanSpeed(action, duration*3600), 2) / height) * walkingSpeedHeightMultiplier * weight) * duration * minInH
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
